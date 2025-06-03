9ccc140fbc8a5979e3f3ed2e384786639d0fce5a
ap3ufersa <ap3ufersa@gmail.com>
2025-06-03 11:52:12 -0300

    Arrumando aula 5

---
+++ unidade2/aula5/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+
+    public static int fibRec(int n) {
+    }
+
+    public static int fibIter(int n) {
+    }
+
+    public static void main(String[] args) {
+        int n = 10; //faça para 20, 50, 100. Comente o tempo e uso de memória.
+        System.out.println("Recursivo: " + fibRecursivo(n));
+        System.out.println("Iterativo: " + fibIterativo(n));
+    }
+}
---
+++ unidade2/aula5/README.md
@@ -0,0 +1,8 @@
+### U2 - Aula 5 - 05/05/2025 - Algoritmos gulosos, Fibonnaci (1,0)
+
+#### Guloso e Fibonnaci 
+
+1. Responda a questão do [ENADE](questaoENADE2017.png).
+
+3. Arrume e teste o [Fibonacci.java](Fibonacci.java).
+
